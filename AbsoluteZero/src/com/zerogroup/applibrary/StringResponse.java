77b86327b84d5e584e65ff5d409e57cd77213379
JackGiuliari <giuliarigi@dei.unipd.it>
2015-11-05 22:23:53 +0100

    Added StringResponse.class
    This is the first of the two classes that will replace SmsOperator,
    maintaining functions and improving flexibility.
    The added class, StringResponse, will be useful to search a pattern into
    a string (an SMS message in our case) and trigger an automated response,
    that is to say another set predefined strings related to the pattern.

---
+++ AbsoluteZero/src/com/zerogroup/applibrary/StringResponse.java
@@ -0,0 +1,77 @@
+package com.zerogroup.applibrary;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+/**
+ * This class operates on a given string.
+ * Its aim is to find a given pattern inside
+ * the string (a "trigger") and to return
+ * a pre-defined String as a "response".
+ * 
+ * @author Giac
+ * @version 1.0
+ */
+
+public class StringResponse {
+
+	private String myString;
+	private Map<String, String> myResponses;
+
+	/**
+	 * Constructor method
+	 * @param aString the String on which the class will operate
+	 */
+	public StringResponse(String aString) {
+		myString = aString;
+		myResponses = new HashMap();
+	}
+	
+	/**
+	 * Finds the strings saved as "triggers"
+	 * inside the given string
+	 * @return an array containing all the "response" strings that were found
+	 */
+	public String[] findResponse() {
+		List<String> toReply = new ArrayList();
+		Scanner scan = new Scanner(myString);
+
+		return (String[]) toReply.toArray();
+	}
+
+	/**
+	 * Adds a new trigger-response pattern that
+	 * will be used by findResponse() method
+	 * @param trigger the key string
+	 * @param response the response that will be sent when the trigger is found in myString
+	 * @return the value of the previous response related to the trigger (if present)
+	 */
+	public String addResponse(String trigger, String response) {
+		return (String) myResponses.put(trigger, response);
+	}
+	
+	/**
+	 * Checks if the trigger is already present
+	 * as an option
+	 * @param trigger
+	 * @return true if trigger is already present, false otherwise
+	 */
+	public boolean hasResponse(String trigger) {
+		return myResponses.containsKey(trigger);
+	}
+
+	/**
+	 * Removes the trigger-response pattern specified
+	 * by the trigger
+	 * @param trigger of the pattern that needs to be removed
+	 * @return the response to the removed trigger(if present)
+	 */
+	public String removeResponse(String trigger) {
+		return (String) myResponses.remove(trigger);
+	}
+}
